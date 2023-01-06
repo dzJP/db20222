609161c927b5765d108520a7f0b10bab9fd3f8cb
dzJP <113700179+dzJP@users.noreply.github.com>
2023-01-06 21:15:01 +0100

    Finishing up normalization

--- normalisering.sql
+++ normalisering.sql
@@ -96,15 +96,65 @@ insert into StudentHobby select distinct Id as StudentId, HobbyId from UNF, Hobb
 /* Grade */
 
 drop table if exists Grade;
-create table Grade (
-StudentId int not null primary key,
-StudentGrade varchar(250) not null
-)engine=innodb;
+DROP TABLE IF EXISTS Grade;
+CREATE TABLE Grade (
+    GradeId INT NOT NULL AUTO_INCREMENT,
+    Name VARCHAR(255) NOT NULL,
+    CONSTRAINT PRIMARY KEY (GradeId)
+)  ENGINE=INNODB;
 
-insert into Grade (StudentId, StudentGrade) select UNF.Id as StudentId, "Awesome" as StudentGrade from UNF where Grade like "%some%"
-union select UNF.Id as StudentId, "First Class" as StudentGrade from UNF where Grade like "%first%"
-union select UNF.Id as StudentId, "Admirable" as StudentGrade from UNF where Grade like "%mirable%"
-union select UNF.Id as StudentId, "Gorgeous" as StudentGrade from UNF where Grade like "%gorg%"
-union select UNF.Id as StudentId, "Best" as StudentGrade from UNF where Grade like "%est%"
-union select UNF.Id as StudentId, "Excellent" as StudentGrade from UNF where Grade like "%lent%"
-union select UNF.Id as StudentId, "Profound" as StudentGrade from UNF where Grade like "%found%";
+INSERT INTO Grade(Name) 
+SELECT DISTINCT Grade FROM UNF;
+
+ALTER TABLE Student ADD COLUMN GradeId INT NOT NULL;
+
+/* Ofta kan komplicerade join i en update leda till att resultatet av join får skrivskydd */
+/* Så just sådana här UPDATE satser kan kräva mycket slit */
+UPDATE Student JOIN UNF ON (StudentID = Id) JOIN Grade ON Grade.Name = UNF.Grade 
+SET  Student.GradeId =  Grade.GradeId;
+
+/* Tänk på hur många rader du vill ha i resultatet, hitta tabellen med så många rader, lägg till nya tabeller med LEFT JOIN hela tiden, och du kommer att sluta med rätt antal rader */
+
+/* GRUND TABELL */
+SELECT StudentId as ID  FROM StudentSchool;
+
+/* LEFT JOIN 1 */
+SELECT StudentId as ID, Student.Name FROM StudentSchool 
+LEFT JOIN Student USING (StudentId);
+
+/* LEFT JOIN 2 */
+SELECT StudentId as ID, Student.Name, Grade.Name AS Grade FROM StudentSchool
+LEFT JOIN Student USING (StudentId)
+LEFT JOIN Grade USING (GradeId);
+
+
+/* LEFT JOIN 3 */
+SELECT StudentId as ID, Student.Name, Grade.Name AS Grade, Hobbies FROM StudentSchool
+LEFT JOIN Student USING (StudentId)
+LEFT JOIN Grade USING (GradeId)
+LEFT JOIN HobbiesList USING (StudentID);
+
+/* LEFT JOIN 4 */
+SELECT StudentId as ID, Student.Name, Grade.Name AS Grade, Hobbies, School.Name AS School, City FROM StudentSchool
+LEFT JOIN Student USING (StudentId)
+LEFT JOIN Grade USING (GradeId)
+LEFT JOIN HobbiesList USING (StudentID)
+LEFT JOIN School USING (SchoolId);
+
+/* LEFT JOIN 5 */
+SELECT StudentId as ID, Student.Name, Grade.Name AS Grade, Hobbies, School.Name AS School, City, Numbers FROM StudentSchool
+LEFT JOIN Student USING (StudentId)
+LEFT JOIN Grade USING (GradeId)
+LEFT JOIN HobbiesList USING (StudentId)
+LEFT JOIN School USING (SchoolId)
+LEFT JOIN PhoneList USING (StudentId);
+
+
+DROP VIEW IF EXISTS AVSLUT;
+CREATE VIEW AVSLUT AS
+SELECT StudentId as ID, Student.Name, Grade.Name AS Grade, Hobbies, School.Name AS School, City, Numbers FROM StudentSchool
+LEFT JOIN Student USING (StudentId)
+LEFT JOIN Grade USING (GradeId)
+LEFT JOIN HobbiesList USING (StudentId)
+LEFT JOIN School USING (SchoolId)
+LEFT JOIN PhoneList USING (StudentId);
