559c9a501935c8a817fd291eef5682e44e317bd3
dzJP <113700179+dzJP@users.noreply.github.com>
2023-01-12 18:18:31 +0100

    Tested and checked that our Spring Boot application works properly

--- app/src/main/java/se/iths/App.java
+++ app/src/main/java/se/iths/App.java
@@ -1,9 +1,13 @@
 package se.iths;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class App {
-  
-  public static void main(String[] args){
-    System.out.println("Hello World"); 
-  }
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class);
+    }
 
 }
